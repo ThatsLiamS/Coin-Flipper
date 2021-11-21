a2b0ab55c9dab1a9902fe2caddcfe5684ef89565
Liam Skinner <liamdavidskinner@gmail.com>
2021-11-21 12:44:48 +0000

    Removed sourcecode

--- src/tools/convertToEmote.js
+++
@@ -1,4 +0,0 @@
-module.exports = function convertToEmote(value) {
-	const emote = value ? "<:true:832294753121861632>" : "<:false:832295611905867816>";
-	return emote;
-};
--- src/tools/outOfOrder.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	execute: async function(firestore, args, command, msg, discord, data, send) {
-		let embed = new discord.MessageEmbed()
-			.setTitle("🚫 Out of Order")
-			.setDescription("Sorry, this command is out of order. It will be back as soon as possible!")
-			.setColor('RED');
-		send(embed);
-	}
-};
