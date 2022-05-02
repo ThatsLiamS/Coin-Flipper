9a918bde16e1cd640da77f5a70f702ce1fa0c6a1
Liam Skinner <liamdavidskinner@gmail.com>
2022-05-02 19:43:04 +0100

    feat: added report and suggestion cmd

---
+++ src/commands/information/report.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed, WebhookClient } = require('discord.js');
+
+module.exports = {
+	name: 'report',
+	description: 'Report a bug/issue to the developers!',
+	usage: '<description>',
+
+	permissions: [],
+	ownerOnly: false,
+	guildOnly: true,
+	developerOnly: false,
+
+	data: new SlashCommandBuilder()
+		.setName('report')
+		.setDescription('Report a bug/issue to the developers!')
+		.addStringOption(option => option
+			.setName('description')
+			.setDescription('Explain the issue you are having')
+			.setRequired(true),
+		),
+
+	error: false,
+	execute: ({ interaction, client }) => {
+
+		const avatarURL = interaction.guild.iconURL() ? interaction.guild.iconURL() : 'https://i.imgur.com/yLv2YVnh.jpg';
+		const embed = new MessageEmbed()
+			.setColor('#0099ff')
+			.setDescription(`**${client.user.tag}**\n${interaction.options.getString('description')}`)
+			.setAuthor({ name: interaction.user.username, iconURL: interaction.user.displayAvatarURL() })
+			.setFooter({ text: `ID: ${interaction.member.id}` })
+			.setTimestamp();
+
+		const webhook = new WebhookClient({ url: process.env['ReportWebhook'] });
+		webhook.send({ username: interaction.guild.name, avatarURL, embeds: [embed] });
+
+		interaction.followUp({ content: 'Thank you for helping us make Coin Flipper even better.', ephemeral: true });
+	},
+};
---
+++ src/commands/information/suggest.js
@@ -0,0 +1,39 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed, WebhookClient } = require('discord.js');
+
+module.exports = {
+	name: 'suggest',
+	description: 'Suggest an improvement, command or feature!',
+	usage: '<description>',
+
+	permissions: [],
+	ownerOnly: false,
+	guildOnly: true,
+	developerOnly: false,
+
+	data: new SlashCommandBuilder()
+		.setName('suggest')
+		.setDescription('Suggest an improvement, command or feature!')
+		.addStringOption(option => option
+			.setName('description')
+			.setDescription('Include a detailed description of your suggestion')
+			.setRequired(true),
+		),
+
+	error: false,
+	execute: ({ interaction, client }) => {
+
+		const avatarURL = interaction.guild.iconURL() ? interaction.guild.iconURL() : 'https://i.imgur.com/yLv2YVnh.jpg';
+		const embed = new MessageEmbed()
+			.setColor('#0099ff')
+			.setDescription(`**${client.user.tag}**\n${interaction.options.getString('description')}`)
+			.setAuthor({ name: interaction.user.username, iconURL: interaction.user.displayAvatarURL() })
+			.setFooter({ text: `ID: ${interaction.member.id}` })
+			.setTimestamp();
+
+		const webhook = new WebhookClient({ url: process.env['SuggestionWebhook'] });
+		webhook.send({ username: interaction.guild.name, avatarURL, embeds: [embed] });
+
+		interaction.followUp({ content: 'Your suggestion has been sent to my developers.', ephemeral: true });
+	},
+};
