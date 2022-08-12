c919a9b6a41ead731ee3b08ec69f2dd8430ac788
Liam Skinner <liamdavidskinner@gmail.com>
2022-08-12 12:56:36 +0100

    feat(Logs): added guild event logs

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,6 @@
 const { Client, GatewayIntentBits, Partials } = require('discord.js');
 const client = new Client({
-	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildWebhooks],
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildWebhooks],
 	partials: [Partials.Channel, Partials.Message, Partials.Reaction],
 });
 
---
+++ src/events/guildDelete.js
@@ -0,0 +1,33 @@
+const { EmbedBuilder, WebhookClient } = require('discord.js');
+
+module.exports = {
+	name: 'guildDelete',
+	once: false,
+
+	execute: async (guild, client) => {
+		if (!guild || !guild?.available) return false;
+
+		const ownerId = guild?.ownerId;
+		const owner = ownerId ? await client.users.fetch(ownerId) : ownerId;
+
+		const avatarURL = guild?.iconURL() ? guild?.iconURL() : 'https://i.imgur.com/yLv2YVnh.jpg';
+		const embed = new EmbedBuilder()
+			.setColor('Red')
+			.setTitle(`${client.user.username} - Left a Server!`)
+			.addFields(
+				{ name: 'Name', value: `${guild?.name}`, inline: true },
+				{ name: 'ID', value: `${guild?.id}`, inline: true },
+				{ name: 'Owner', value: `${owner || 'Unknown User'}`, inline: true },
+
+				{ name: 'Member Count', value: `${guild?.memberCount} / ${guild.maximumMembers}`, inline: true },
+				{ name: 'Created At', value: `${guild?.createdAt}`, inline: true },
+				{ name: 'Location', value: `${guild?.preferredLocale || 'Unknown Location'}`, inline: true },
+			)
+			.setAuthor({ name: guild?.name, iconURL: avatarURL })
+			.setFooter({ text: 'Filter keywords: Coin Flipper, guildDelete, Guild, Left, Delete' })
+			.setTimestamp();
+
+		const webhook = new WebhookClient({ url: process.env['DeveloperLogs'] });
+		webhook.send({ username: client.user.username, avatarURL: client.user.displayAvatarURL(), embeds: [embed] });
+	},
+};
