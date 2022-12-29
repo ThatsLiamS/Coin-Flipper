e2b67f05ac8091c6cfd50d0667f341d23a620c73
Liam Skinner <Liamdavidskinner@gmail.com>
2022-12-29 17:55:21 +0000

    refactor(Schema): created new database model and schema

---
+++ src/util/Database Schema.js
@@ -0,0 +1,67 @@
+const AddonSchema = {
+	name: '', description: '', cost: 0,
+	published: false, responses: [],
+};
+
+
+const UserSchema = {
+
+	stats: {
+		flips: 0,
+		minigames: 0,
+		worked: 0,
+		job: '',
+
+		donator: 0,
+		balance: 0,
+		bank: 0,
+		lifeEarnings: 0,
+	},
+
+	settings: {
+		evil: false,
+		compact: false,
+		banned: false,
+		developer: false,
+	},
+
+	cooldowns: {
+		daily: '',
+		weekly: '',
+		monthly: '',
+	},
+
+	addons: [
+		/**
+		 * @example [AddonSchema, AddonSchema, AddonSchema]
+		**/
+	],
+
+	items: {},
+	badges: {},
+	achievements: {},
+
+};
+
+const GuildSchema = {
+
+	enabled: {
+		trash: true,
+		minigames: true,
+		addons: true,
+	},
+
+	addons: [
+		/**
+		 * @example [AddonSchema, AddonSchema, AddonSchema]
+		**/
+	],
+
+};
+
+
+module.exports = {
+	AddonSchema,
+	UserSchema,
+	GuildSchema,
+};
